20097facac72f61f6f11c2406cbdd6b25b761746
zetsux <kevnhalim@gmail.com>
2024-06-20 09:29:26 +0700

    feat: add id model

--- app.py
+++ app.py
@@ -10,48 +10,72 @@ import numpy as np
 app = FastAPI()
 
 # Load the trained model
-model = tf.keras.models.load_model("model/chatbot_engmodel.h5")
+enModel = tf.keras.models.load_model("model/chatbot_engmodel.h5")
+idModel = tf.keras.models.load_model("model/chatbot_indomodel.h5")
 
 # Load the tokenizer and label encoder
-with open("util/tokenizer.pickle", "rb") as handle:
-    tokenizer = pickle.load(handle)
-with open("util/label_encoder.pickle", "rb") as enc_file:
-    lbl_encoder = pickle.load(enc_file)
-with open("data/engdataset.json", "r") as data_file:
-    data1 = json.load(data_file)
+with open("util/en_tokenizer.pickle", "rb") as handle:
+    en_tokenizer = pickle.load(handle)
+with open("util/en_label_encoder.pickle", "rb") as enc_file:
+    en_lbl_encoder = pickle.load(enc_file)
+with open("util/id_tokenizer.pickle", "rb") as handle2:
+    id_tokenizer = pickle.load(handle2)
+with open("util/id_label_encoder.pickle", "rb") as enc_file2:
+    id_lbl_encoder = pickle.load(enc_file2)
+with open("data/engdataset.json", "r") as data1_file:
+    enData = json.load(data1_file)
+with open("data/indodataset.json", "r") as data2_file:
+    idData = json.load(data2_file)
 
 
 # Initialize SymSpell for typo correction
 sym_spell = SymSpell(max_dictionary_edit_distance=2, prefix_length=7)
 
 
-def correct_typo(input_text):
-    suggestions = sym_spell.lookup_compound(input_text, max_edit_distance=2)
+def correct_typo(input):
+    suggestions = sym_spell.lookup_compound(input, max_edit_distance=2)
     # Return the corrected input if any suggestions exist
     if suggestions:
         return suggestions[0].term
-    return input_text
+    return input
 
 
 @app.post("/")
-def chat(input_text: str):
+def chat(language: str, input: str):
     # Correct typos in user input
-    corrected_input = correct_typo(input_text)
+    corrected_input = correct_typo(input)
 
-    # Process the corrected input
-    result = model.predict(
-        pad_sequences(
-            tokenizer.texts_to_sequences([corrected_input]),
-            truncating="post",
-            maxlen=18,
+    if language == "id":
+        # Process the corrected input
+        result = idModel.predict(
+            pad_sequences(
+                id_tokenizer.texts_to_sequences([corrected_input]),
+                truncating="post",
+                maxlen=6,
+            )
         )
-    )
-    tag = lbl_encoder.inverse_transform([np.argmax(result)])
+        tag = id_lbl_encoder.inverse_transform([np.argmax(result)])
 
-    for intent in data1["intents"]:
-        if intent["tag"] == tag:
-            resp = np.random.choice(intent["responses"])
-            print("Chatbot: ", resp)
-            return {"response": resp}
+        for intent in idData["intents"]:
+            if intent["tag"] == tag:
+                resp = np.random.choice(intent["responses"])
+                print("Chatbot: ", resp)
+                return {"response": resp}
+    else:
+        # Process the corrected input
+        result = enModel.predict(
+            pad_sequences(
+                en_tokenizer.texts_to_sequences([corrected_input]),
+                truncating="post",
+                maxlen=18,
+            )
+        )
+        tag = en_lbl_encoder.inverse_transform([np.argmax(result)])
+
+        for intent in enData["intents"]:
+            if intent["tag"] == tag:
+                resp = np.random.choice(intent["responses"])
+                print("Chatbot: ", resp)
+                return {"response": resp}
 
     return {"response": ""}
